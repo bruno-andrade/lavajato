dde29c704a10c554fa465cd320933fb686504019
Bruno Andrade <brunoandrade1209@outlook.com>
2021-03-28 12:48:19 -0300

    ajuste sw

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('js/sw.js').then(function(registration) {
         
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log('1- ServiceWorker registration successful with scope: ', registration);
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
