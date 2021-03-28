fb30ea821ab03593538fb3819c781d7c0b6e3e96
Bruno Andrade <brunoandrade1209@outlook.com>
2021-03-28 14:17:34 -0300

    ajuste no service work / colocado na raiz

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('js/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
         
         console.log('1- ServiceWorker registration successful with scope: ', registration);
       }, function(err) {
